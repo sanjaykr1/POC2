2188e349fa85376fb4f89ce5fed4462d3d50dfb0
sanjaykr1 <sanjaykr19109@gmail.com>
2022-02-11 13:29:45 +0100

    Implemented grouping of connected components

--- pipeline.py
+++ pipeline.py
@@ -1,8 +1,17 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import struct
 from pyspark.sql.types import *
 from pyspark.sql import functions as f
 import networkx
-from networkx import Graph
+from networkx import Graph,draw
+
+
+def udf1(row):
+    for k, v in l1.items():
+        if row['ORIG'] in v:
+            # print(k)
+            return k
+
 
 new_schema = StructType([
     StructField("REF_ID", StringType(), True),
@@ -24,12 +33,23 @@ new_schema = StructType([
 ])
 
 spark = SparkSession.builder.appName("POC2").master("local").getOrCreate()
-df1 = spark.read.option("header", True).csv("dataset/mockdata.csv", schema=new_schema)
+df1 = spark.read.option("header", True).csv("dataset/data.csv", schema=new_schema)
 # df1.show(truncate=False)
 
 df2 = df1.filter(f.col("TOTAL_Score") > 15)
 # print(df2.count())
 
 graph1 = Graph()
-graph1.add_nodes_from(list(df1["ORIG"], df1["BENEF"]))
-print(networkx.number_connected_components(graph1))
+pandas_df = df2.toPandas()
+orig = tuple(pandas_df['ORIG'])
+benef = tuple(pandas_df['BENEF'])
+graph1.add_edges_from(list(zip(orig, benef)))
+l1 = {}
+count = 0
+for comp in networkx.connected_components(graph1):
+    l1[count] = list(comp)
+    count += 1
+
+udf1 = f.udf(udf1, IntegerType())
+df2 = df2.withColumn("Group", udf1(struct(df2["ORIG"])))
+df2.show(25, truncate=False)
